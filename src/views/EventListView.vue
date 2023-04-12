39ece6b47a1a993c272ed9ed53df5f183c6c9cc6
daviddpi <piscopodavid97@gmail.com>
2023-04-12 17:26:55 +0200

    change: use computed

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted, watchEffect } from 'vue'
+import { ref, onMounted, watchEffect, computed } from 'vue'
 import EventCard from '@/components/EventCard.vue'
 import EventService from '@/services/EventService.js'
 
@@ -12,19 +12,23 @@ const props = defineProps({
 
 const events = ref(null)
 const perPage = 2
-let totalPage = ref(null)
+const totalEvent = ref(null)
 
 onMounted(() => {
-  watchEffect( () =>{
+  watchEffect(() => {
     EventService.getEvents(perPage, props.page)
       .then((response) => {
         events.value = response.data
-        totalPage.value = response.headers['x-total-count'] / perPage
+        totalEvent.value = response.headers['x-total-count']
       })
       .catch((error) => {
         console.log(error)
       })
   })
+})
+
+const totalPage = computed(() => {
+  return totalEvent.value / perPage
 })
 </script>
 
@@ -32,18 +36,21 @@ onMounted(() => {
   <h1>Events For Good</h1>
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
-
-    <router-link
-      :to="{ name: 'event-list', query: { page: page - 1 } }"
-      rel="prev"
-      v-if="page != 1"
-      >Previus</router-link
-    >
-    <router-link
-      :to="{ name: 'event-list', query: { page: page + 1 } }"
-      rel="next"
-      v-if="page < totalPage">Next</router-link
-    >
+    <div class="pagination">
+      <router-link
+        :to="{ name: 'event-list', query: { page: page - 1 } }"
+        rel="prev"
+        v-if="page != 1"
+        class="prev">Previus</router-link
+      >
+      <router-link
+        :to="{ name: 'event-list', query: { page: page + 1 } }"
+        rel="next"
+        v-if="page < totalPage"
+        class="next"
+        >Next</router-link
+      >
+    </div>
   </div>
 </template>
 
@@ -52,5 +59,19 @@ onMounted(() => {
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  margin-bottom: 25px;
+}
+
+.pagination{
+  display: flex;
+  justify-content: space-around;
+  width: 280px;
+}
+
+.pagination a{
+  text-decoration: none;
+   font-size: 21px;
+   color: #353a34;
 }
 </style>

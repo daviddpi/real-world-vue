efc753b6d798510eded11cf3bb0caf991e4983e3
daviddpi <piscopodavid97@gmail.com>
2023-04-12 17:16:52 +0200

    feat: pagination

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -1,18 +1,30 @@
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, watchEffect } from 'vue'
 import EventCard from '@/components/EventCard.vue'
 import EventService from '@/services/EventService.js'
 
+const props = defineProps({
+  page: {
+    type: Number,
+    required: true,
+  },
+})
+
 const events = ref(null)
+const perPage = 2
+let totalPage = ref(null)
 
 onMounted(() => {
-  EventService.getEvents()
-    .then((response) => {
-      events.value = response.data
-    })
-    .catch((error) => {
-      console.log(error)
-    })
+  watchEffect( () =>{
+    EventService.getEvents(perPage, props.page)
+      .then((response) => {
+        events.value = response.data
+        totalPage.value = response.headers['x-total-count'] / perPage
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  })
 })
 </script>
 
@@ -20,6 +32,18 @@ onMounted(() => {
   <h1>Events For Good</h1>
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+
+    <router-link
+      :to="{ name: 'event-list', query: { page: page - 1 } }"
+      rel="prev"
+      v-if="page != 1"
+      >Previus</router-link
+    >
+    <router-link
+      :to="{ name: 'event-list', query: { page: page + 1 } }"
+      rel="next"
+      v-if="page < totalPage">Next</router-link
+    >
   </div>
 </template>
 
